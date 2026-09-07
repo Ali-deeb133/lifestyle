914760c8d36963201c531da65e90e33ca0f1574d
Ali-deeb133 <deeb10957@gmail.com_Ali-deeb133>
2026-09-07 12:29:09 +0300

    Initial commit: Lifestyle.ai backend setup 1

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'lifestyle-backend-05r7.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 # JWT
 
